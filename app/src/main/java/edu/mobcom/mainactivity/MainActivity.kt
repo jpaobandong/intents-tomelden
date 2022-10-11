b37c01af93693e1628021ec4f4eec8b6d3875374
Juan Paolo Bandong <juan.bandong@awsys-i.com>
2022-10-11 22:11:45 +0800

    Initial commit

---
+++ app/src/main/java/edu/mobcom/mainactivity/MainActivity.kt
@@ -0,0 +1,59 @@
+package edu.mobcom.mainactivity
+
+import android.content.ActivityNotFoundException
+import android.content.Intent
+import android.net.Uri
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val webBtn: Button = findViewById(R.id.btnWeb)
+        val mailBtn: Button = findViewById(R.id.btnMail)
+        val toastBtn: Button = findViewById(R.id.btnToast)
+        val nxtBtn: Button = findViewById(R.id.btnNext)
+        val dialBtn: Button = findViewById(R.id.btnDial)
+
+        nxtBtn.setOnClickListener {
+            val i = Intent(this, MainActivity2::class.java)
+            startActivity(i)
+        }
+
+        webBtn.setOnClickListener {
+            val strURL = "https://www.ubaguio.edu"
+            val iBrowser = Intent(Intent.ACTION_VIEW, Uri.parse(strURL))
+            startActivity(iBrowser)
+        }
+
+        dialBtn.setOnClickListener {
+            val iDialer = Intent(Intent.ACTION_DIAL)
+            startActivity(iDialer)
+        }
+
+        //send email
+        val txtMsg = "Hello there"
+        val errMsg = "No message found."
+        val iMessage = Intent().apply {
+            action = Intent.ACTION_SEND
+            putExtra(Intent.EXTRA_TEXT, txtMsg)
+            type = "text/plain"
+        }
+
+        mailBtn?.setOnClickListener {
+            try{
+                startActivity(iMessage)
+            }catch(e: ActivityNotFoundException){
+                Toast.makeText(this@MainActivity, errMsg, Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        toastBtn.setOnClickListener {
+            Toast.makeText(this@MainActivity, "Hello I am a disguised Toast", Toast.LENGTH_SHORT).show()
+        }
+    }
+}
